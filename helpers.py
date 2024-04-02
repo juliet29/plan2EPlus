df39b1a84dc4b1bfb051ecd641ae1bde12735ed5
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2024-04-02 07:29:44 -0700

    start git

---
+++ helpers.py
@@ -0,0 +1,44 @@
+from geomeppy import IDF
+import sys
+import os
+
+ENERGY_PLUS_LOCATION = "../../../../../Applications/EnergyPlus-22-2-0"
+IDD_FILE = f"{ENERGY_PLUS_LOCATION}/Energy+.idd"
+IDF_FILE = f"{ENERGY_PLUS_LOCATION}/ExampleFiles/Minimal.idf"
+
+WEATHER_FILE = "weather/CA_PALO-ALTO-AP_724937S_20.epw"
+
+IDF.setiddname(IDD_FILE)
+
+
+def new_idf():
+    idf = IDF(IDF_FILE)
+    idf.epw = WEATHER_FILE
+
+    return idf
+
+def print_idf_in_file(idf:IDF, file="temp_idf"):
+    sys.stdout = open(f'{file}.txt','wt')
+    print(idf.idfstr())
+
+    return
+
+class EneryPlusCase:
+    def __init__(self, case_name:str) -> None:
+        # make case folder
+        self.path = os.path.join("cases", case_name)
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+        
+        # initialize starting IDF File 
+        self.idf = IDF(IDF_FILE)
+        self.idf.epw = WEATHER_FILE
+
+    def save_idf(self):
+        # TODO what if there are multiple saves (and runs..)
+        self.idf.save(filename=os.path.join(self.path, "out.idf"))
+
+    def run_idf(self):
+        self.idf.run(output_directory=os.path.join(self.path, "results"))
+
+
