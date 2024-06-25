047149e5041f6a67440b10e7a3e22ad7f706a740
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2024-06-25 06:35:29 -0700

    subsurface geometry

--- _scripts/case_edits/object_getter.py
+++ _scripts/case_edits/object_getter.py
@@ -5,10 +5,17 @@ from case_edits.methods.subsurfaces.inputs import SubsurfaceObjects
 
 class Getter:
     def __init__(self, epcase) -> None:
+        # TODO epcase will be actual case or idf.. 
         self.epcase = epcase
+        try:
+            self.idfobjects = self.epcase.idf.idfobjects
+        except:
+            self.idfobjects = self.epcase.idfobjects
         self.subsurfaces = []
 
     def get_original_subsurfaces(self):
+        if not self.subsurfaces:
+            self.get_subsurfaces()
         self.original_subsurfaces = [s for s in self.subsurfaces if "Partner" not in s.Name]
         return self.original_subsurfaces
 
@@ -22,7 +29,7 @@ class Getter:
 
 
     def get_subsurface_by_type(self, pattern):
-        for k,v in self.epcase.idf.idfobjects.items():
+        for k,v in self.idfobjects.items():
             if bool(pattern.match(k)):
                 if len(v) > 0:
                     for item in v:
@@ -33,7 +40,7 @@ class Getter:
     def get_afn_objects(self):
         self.afn_objects = []
         pattern = re.compile("AIRFLOWNETWORK")
-        for k,v in self.epcase.idf.idfobjects.items():
+        for k,v in self.idfobjects.items():
             if bool(pattern.match(k)):
                 if len(v) > 0:
                     for item in v:
--- _scripts/geometry/subsurface.py
+++ _scripts/geometry/subsurface.py
@@ -1,20 +1,31 @@
 from geomeppy.patches import EpBunch
 import shapely as sp
+from shapely import LineString
+
+
 
 
 class Subsurface:
     def __init__(self, idf_data: EpBunch, wall) -> None:
         self.data = idf_data
-        self.name = idf_data.Name
         self.wall = wall
-        self.type = "" # object category or something.. 
 
-        self.line: sp.LineString
+        self.create_display_name()
+        self.get_geometry()
+
+    def __repr__(self):
+        return f"Subsurface({self.display_name})"
 
     def create_display_name(self):
-        self.display_name = f"{self.type} on {self.wall.display_name}"
+        self.name = self.data.Name
+        self.object_type = self.data.key.title()
+        self.display_name = f"{self.object_type} on {self.wall.display_name}"
         pass
 
     def get_geometry(self):
         self.start_x = self.data.Starting_X_Coordinate
         self.length = self.data.Length
+        line = self.wall.line
+        start =line.line_interpolate_point(self.start_x)
+        end = line.line_interpolate_point(self.start_x+self.length)
+        self.line = LineString([start, end])
