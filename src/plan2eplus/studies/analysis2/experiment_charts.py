b0442b9560891a26a042f8fac39520f628a209b5
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2025-01-25 12:55:29 -0800

    tables created for all exp

--- src/plan2eplus/studies/analysis2/experiment_charts.py
+++ src/plan2eplus/studies/analysis2/experiment_charts.py
@@ -1,51 +1,16 @@
-import functools
 import polars as pl
 import altair as alt
 
-from plan2eplus.helpers.variable_interfaces import all_variables as vars
 from plan2eplus.helpers.dates import create_save_details
-from plan2eplus.studies.analysis2.all_cases_v_time import create_space_and_site_dfs
 from plan2eplus.studies.experiments.name_splits import (
     get_chart_details,
-    split_by_case_type_and_alias,
-    split_by_doors,
-    split_by_materials,
-    split_by_windows,
 )
-from plan2eplus.studies.experiments.scatter import get_split_fx
-from plan2eplus.studies.setup.data_wrangle2 import (
-    create_wide_dataframe_for_many_qois_and_cases,
-)
-from ..experiments.retrieve import COMPARISON_GROUPS, retrieve_comparison_groups
+from plan2eplus.studies.experiments.name_splits import create_and_split_data_by_exp
+from ..experiments.retrieve import COMPARISON_GROUPS
 
 
 def get_save_details():
     return create_save_details("exp_res2")
-
-@functools.lru_cache
-def create_and_split_data_by_exp(comparison_group: COMPARISON_GROUPS):
-    # TODO => could this be created in a neater way?
-    cases = retrieve_comparison_groups(comparison_group)
-    df, _ = create_space_and_site_dfs(
-        cases=cases,
-        space_qois=[vars.afn.zone["vent_vol"], vars.afn.zone["mix_vol"]],
-        site_qois=[vars.site.wind["speed"]],
-    )
-    df_temp = create_wide_dataframe_for_many_qois_and_cases(
-        cases, [vars.zone.temp["zone_mean_air_temp"]]
-    )
-    df_vol = df.join(df_temp, on=["case_names", "space_names", "datetimes"])
-
-    df_agg = df_vol.group_by("case_names").agg(
-        [
-            pl.mean("AFN Zone Ventilation Volume [m3]"),
-            pl.mean("AFN Zone Mixing Volume [m3]"),
-            pl.mean("Zone Mean Air Temperature [C]"),
-        ]
-    )
-    df_split = split_by_case_type_and_alias(df_agg)
-    split_fx = get_split_fx(comparison_group)
-    return split_fx(df_split)
 
 
 def create_exp_chart(
@@ -79,7 +44,6 @@ def create_exp_chart(
 
 
 def save_charts_for_all_groups():
-    for g in ['windows', 'materials', 'doors']:
+    for g in ["windows", "materials", "doors"]:
         df = create_and_split_data_by_exp(g)
         create_exp_chart(df, g, save_chart=True)
-
--- src/plan2eplus/studies/experiments/name_splits.py
+++ src/plan2eplus/studies/experiments/name_splits.py
@@ -1,8 +1,13 @@
 from typing import Callable
 import polars as pl
-
+import functools
+from plan2eplus.helpers.variable_interfaces import all_variables as vars
+from plan2eplus.studies.analysis2.all_cases_v_time import create_space_and_site_dfs
+from plan2eplus.studies.experiments.retrieve import retrieve_comparison_groups
+from plan2eplus.studies.setup.data_wrangle2 import (
+    create_wide_dataframe_for_many_qois_and_cases,
+)
 from .retrieve import COMPARISON_GROUPS
-
 
 
 def split_by_case_type_and_alias(df: pl.DataFrame) -> pl.DataFrame:
@@ -45,7 +50,9 @@ def split_by_windows(df: pl.DataFrame) -> pl.DataFrame:
     )
 
 
-def get_split_fx(comparison_group: COMPARISON_GROUPS) -> Callable[[pl.DataFrame], pl.DataFrame]:
+def get_split_fx(
+    comparison_group: COMPARISON_GROUPS,
+) -> Callable[[pl.DataFrame], pl.DataFrame]:
     match comparison_group:
         case "doors":
             return split_by_doors
@@ -67,3 +74,43 @@ def get_chart_details(comparison_group: COMPARISON_GROUPS):
             return ("Material Type", ["Light", "Medium", "Heavy"])
         case _:
             raise Exception("Invalid group ")
+        
+
+def get_table_details(comparison_group: COMPARISON_GROUPS):
+    match comparison_group:
+        case "doors":
+            control = "OPEN"
+        case "windows":
+            control = "Control"
+        case "materials":
+            control = "Medium"
+        case _:
+            raise Exception("Invalid group ")
+    table_name = f"{comparison_group.title()} Experiment"
+    return (control, table_name)
+
+
+@functools.lru_cache
+def create_and_split_data_by_exp(comparison_group: COMPARISON_GROUPS):
+    # TODO => could this be created in a neater way?
+    cases = retrieve_comparison_groups(comparison_group)
+    df, _ = create_space_and_site_dfs(
+        cases=cases,
+        space_qois=[vars.afn.zone["vent_vol"], vars.afn.zone["mix_vol"]],
+        site_qois=[vars.site.wind["speed"]],
+    )
+    df_temp = create_wide_dataframe_for_many_qois_and_cases(
+        cases, [vars.zone.temp["zone_mean_air_temp"]]
+    )
+    df_vol = df.join(df_temp, on=["case_names", "space_names", "datetimes"])
+
+    df_agg = df_vol.group_by("case_names").agg(
+        [
+            pl.mean("AFN Zone Ventilation Volume [m3]"),
+            pl.mean("AFN Zone Mixing Volume [m3]"),
+            pl.mean("Zone Mean Air Temperature [C]"),
+        ]
+    )
+    df_split = split_by_case_type_and_alias(df_agg)
+    split_fx = get_split_fx(comparison_group)
+    return split_fx(df_split)
