58d046026c55569d0c4db57c39d3ffa0cc11110c
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2025-07-26 13:29:20 -0700

    move studies out

--- src/plan2eplus/connectivity_study/plot.py
+++
@@ -1,9 +0,0 @@
-from plan2eplus.studies.analysis.plot_helpers import plot_zone_domains
-import matplotlib.pyplot as plt
-from geomeppy import IDF
-
-def plot_case(idf: IDF):
-    fig, ax = plt.subplots()
-    plot_zone_domains(idf, ax)
-    plt.show()
-    
